e8b9d0c7fa21d05f6f89606eaa8285a8f3cac8e1
parrotflakes <gdrakemail@gmail.com>
2018-10-18 22:09:14 -0400

    Major clean-up, genCoords is now adaptable to row #

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -4,37 +4,32 @@ let coordinates = [];
 let x = 0;
 let y = 0;
 
+function subtractIfMax(row, y) { // Once y reaches it's max, create respective coordinates until y = 0
+  for (let b = 0; b < row; b++) {
+    y--;
+    x++;
+    coordinates.push([x, y]);
+  }
+  return y;
+}
+
 function generateCoordinates(string, row) { // Generates coordinates according to number of rows and string length
   string = string.toUpperCase(); // Makes string all upper case
+  coordinates.push([x, y]);
   while (x <= string.length && y <= row) {
-    coordinates.push([x, y]);
-    x++; // Always adds to x
-    if (y > row && x !== string.length) {
-      console.log('ERROR: coordinates have exceeded the row limitation.');
-      break;
-    } else if (y == row) {
-      y--;
-      coordinates.push([x, y]);
-      x++;
-      y--; // Loop immediately logs this new coordinate pair
-      if (y > row && x !== string.length) { // AFTER LINE 15, check if y is out of bounds
-        console.log('ERROR: coordinates have exceeded the row limitation.');
-        break;
-      } else {
-        coordinates.push([x, y]);
-        x++;
-        y--;
-      }
+    if (y == row) {
+      y = subtractIfMax(row, y);
     } else {
+      x++;
       y++;
+      coordinates.push([x, y]);
     }
   }
 }
 
-// Now define the index of (0,0), (1,0), (2,0) etc and print the according character in the string
-// If row !== 3, everything breaks
 
+// Now define the index of (0,0), (1,0), (2,0) etc and print the according character in the string [ EVEN POSSIBLE? ]
 
-generateCoordinates('Hi there', 3);
+generateCoordinates('Hello there', 3);
 
 console.log(coordinates); // Logs coordinates for debug purposes
