187a94627f7edd3bbea0a2ccad79b479ce81abfa
parrotflakes <gdrakemail@gmail.com>
2018-10-19 16:53:26 -0400

    Fixed y coordinate system, vertical coordinates now accomodate for zero

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -5,7 +5,7 @@ let x = 0;
 let y = 0;
 
 function subtractIfMax(row, y) { // Once y reaches it's max, create respective coordinates until y = 0
-  for (let b = 0; b < row; b++) {
+  for (let b = 0; b < (row - 1); b++) {
     y--;
     x++;
     coordinates.push([x, y]);
@@ -16,8 +16,8 @@ function subtractIfMax(row, y) { // Once y reaches it's max, create respective c
 function generateCoordinates(string, row) { // Generates coordinates according to number of rows and string length
   string = string.toUpperCase(); // Makes string all upper case
   coordinates.push([x, y]);
-  while (x <= string.length && y <= row) {
-    if (y == row) {
+  while (x <= string.length - 1 && y <= row) {
+    if (y == (row - 1)) {
       y = subtractIfMax(row, y);
     } else {
       x++;
@@ -27,9 +27,8 @@ function generateCoordinates(string, row) { // Generates coordinates according t
   }
 }
 
-
 // Now define the index of (0,0), (1,0), (2,0) etc and print the according character in the string [ EVEN POSSIBLE? ]
 
-generateCoordinates('Hello there', 3);
+generateCoordinates('Hello there', 4);
 
 console.log(coordinates); // Logs coordinates for debug purposes
