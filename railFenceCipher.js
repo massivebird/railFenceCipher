d0916607115c90a654feb2570c01a155ee39e993
parrotflakes <gdrakemail@gmail.com>
2018-10-19 17:44:47 -0400

    Fixed issue where number of coord pairs did not match string length

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -4,11 +4,15 @@ let coordinates = [];
 let x = 0;
 let y = 0;
 
-function subtractIfMax(row, y) { // Once y reaches it's max, create respective coordinates until y = 0
+function subtractIfMax(row, y, string) { // Once y reaches it's max, create respective coordinates until y = 0
   for (let b = 0; b < (row - 1); b++) {
     y--;
     x++;
-    coordinates.push([x, y]);
+    if (x >= string.length) {
+      break;
+    } else {
+      coordinates.push([x, y]);
+    }
   }
   return y;
 }
@@ -16,30 +20,32 @@ function subtractIfMax(row, y) { // Once y reaches it's max, create respective c
 function getCoordinateX(coordinatePair) {
   let coordX = coordinatePair.charAt(0); // coordX = x coordinate
   coordX = parseInt(coordX, 10); // Converts number in string into a manipulable value
-  console.log(coordX);
   return coordX;
 }
 
 function getCoordinateY(coordinatePair) {
   let coordY = coordinatePair.charAt(coordinatePair.length - 1); // coordY = y coordinate
   coordY = parseInt(coordY, 10);
-  console.log(coordY);
   return coordY;
 }
 
 function generateCoordinates(string, row) { // Generates coordinates according to number of rows and string length
   string = string.toUpperCase(); // Makes string all upper case
   coordinates.push([x, y]);
-  while (x <= string.length - 1 && y <= row) {
+  while (x < string.length - 1) {
     if (y == (row - 1)) {
-      y = subtractIfMax(row, y);
+      y = subtractIfMax(row, y, string);
     } else {
       x++;
       y++;
-      coordinates.push([x, y]);
+      if (x >= string.length) {
+        break;
+      } else {
+        coordinates.push([x, y]);
+      }
     }
   }
-  let coordinatePair = coordinates[4].toString(); // Needs a 'for' statement to generate numbers between 0 and (coordinates.length) 
+  let coordinatePair = coordinates[0].toString(); // Needs a 'for' statement to generate numbers between 0 and (coordinates.length) 
   let coordX = getCoordinateX(coordinatePair); // Fetches x coordinate of current pair
   let coordY = getCoordinateY(coordinatePair); // Fetches y coordinate
   console.log(coordinates);
@@ -47,4 +53,4 @@ function generateCoordinates(string, row) { // Generates coordinates according t
 
 // Now define the index of (0,0), (1,0), (2,0) etc and print the according character in the string [ EVEN POSSIBLE? ]
 
-generateCoordinates('Hello there', 3);
+generateCoordinates('seagSMUSH', 3);
